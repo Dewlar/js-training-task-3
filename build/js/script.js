7d3e280cba010c0472a0873a4408f1a6337efc7d
Andrei Markouski <insawm@gmail.com>
2021-10-29 16:45:45 +0300

    added getColor function

--- build/js/script.js
+++ build/js/script.js
@@ -1,7 +1,7 @@
-let date = new Date();
+let currentDate = new Date();
 const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-let month = date.getMonth();
-let year = date.getFullYear();
+let month = currentDate.getMonth();
+let year = currentDate.getFullYear();
 
 htmlCalendar(month, year);
 
@@ -12,39 +12,39 @@ function htmlCalendar(month, year) {
   calendarMonth.textContent = monthNames[month];
 
   let calendarDayList = document.querySelector('.calendar').querySelectorAll('td');
-  let locDate = new Date(year, month, 1);
-  let day = locDate.getDate() - locDate.getDay() + 1;
+  let date = new Date(year, month, 1);
+  let day = date.getDate() - date.getDay() + 1;
   let i = 0;
 
   for (i; i < 35; i++) {
-    locDate.setFullYear(year, month, day++);
-    calendarDayList[i].textContent = locDate.getDate();
-
-    if (locDate.getDay() === 0 || locDate.getDay() === 6) calendarDayList[i].style.color = 'red';
-    else if (month != locDate.getMonth()) calendarDayList[i].style.color = 'blue';
-    else calendarDayList[i].style.color = 'black';
+    date.setFullYear(year, month, day++);
+    calendarDayList[i].textContent = date.getDate();
+    calendarDayList[i].style.color = getColor(date,month);
   }
 
-  locDate.setDate(day);
-  if (month === locDate.getMonth()) {
+  date.setDate(day);
+  if (month === date.getMonth()) {
     let newRow = document.createElement('tr');
     newRow.innerHTML = '<tr><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>';
     document.querySelector('.calendar').querySelector('table').append(newRow);
     calendarDayList = document.querySelector('.calendar').querySelectorAll('td');
 
     for (i; i < 42; i++) {
-      locDate.setFullYear(year, month, day++);
-      calendarDayList[i].textContent = locDate.getDate();
-
-      if (locDate.getDay() === 0 || locDate.getDay() === 6) calendarDayList[i].style.color = 'red';
-      else if (month != locDate.getMonth()) calendarDayList[i].style.color = 'blue';
-      else calendarDayList[i].style.color = 'black';
+      date.setFullYear(year, month, day++);
+      calendarDayList[i].textContent = date.getDate();
+      calendarDayList[i].style.color = getColor(date,month);
     }
   } else {
     if (document.querySelector('.calendar').querySelector('table').rows.length === 7) {
       document.querySelector('.calendar').querySelector('table').deleteRow(6);
     }
   }
+}
+
+function getColor(date, month){
+    if (date.getDay() === 0 || date.getDay() === 6) return 'red';
+      else if (month != date.getMonth()) return 'blue';
+      else return 'black';
 }
 
 document.querySelector('.previous-month').addEventListener('click', () => {
@@ -55,6 +55,7 @@ document.querySelector('.previous-month').addEventListener('click', () => {
   }
   htmlCalendar(month, year);
 });
+
 document.querySelector('.next-month').addEventListener('click', () => {
   month += 1;
   if (month > 11) {
